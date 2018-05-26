7f120964d9ea9080947ee65983b3cfd02157f743
SlenderPlays <georgemagic10@gmail.com>
2018-05-26 20:24:07 +0300

    Fixed formating

--- mutliadmin/MultiAdmin/Program.cs
+++ mutliadmin/MultiAdmin/Program.cs
@@ -76,35 +76,35 @@ namespace MutliAdmin
 			}
 			else
 			{
-                // The first check sees if the "servers" directory exists, and if it does, 
-                //  the second check will see if it is empty.
-                if (Directory.Exists(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "servers") &&
-                    HasSubdirs(Directory.GetDirectories(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "servers")))
-                {
-                    Write("Using multiple server mode", ConsoleColor.Green);
-                    multiMode = true;
-                    hasServerToStart = LoadserverFolders();
-                }
-                else
-                {
-                    // Either there is no "servers" folder or it is empty, and starting a normal server
-                    multiMode = false;
-                    hasServerToStart = true;
-                    Write("Using default server mode", ConsoleColor.Green);
-                    Write("Server directory not found or it is empty, if you want to use multiple server mode, please make a new directory in the following format:", ConsoleColor.Yellow);
-                    Write(Directory.GetCurrentDirectory() + "\\servers\\<Server id>\\config.txt", ConsoleColor.Yellow);
-                }
-            }
+				// The first check sees if the "servers" directory exists, and if it does, 
+				//  the second check will see if it is empty.
+				if (Directory.Exists(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "servers") &&
+					HasSubdirs(Directory.GetDirectories(Directory.GetCurrentDirectory() + Path.DirectorySeparatorChar + "servers")))
+				{
+					Write("Using multiple server mode", ConsoleColor.Green);
+					multiMode = true;
+					hasServerToStart = LoadserverFolders();
+				}
+				else
+				{
+					// Either there is no "servers" folder or it is empty, and starting a normal server
+					multiMode = false;
+					hasServerToStart = true;
+					Write("Using default server mode", ConsoleColor.Green);
+					Write("Server directory not found or it is empty, if you want to use multiple server mode, please make a new directory in the following format:", ConsoleColor.Yellow);
+					Write(Directory.GetCurrentDirectory() + "\\servers\\<Server id>\\config.txt", ConsoleColor.Yellow);
+				}
+			}
 
 			if (!hasServerToStart)
 			{
 				Write("All servers are set to manual start! you should have at least one config that auto starts", ConsoleColor.Red);
 			}
-
+			
 			return hasServerToStart;
 		}
 
-        public static bool HasSubdirs(string[] dirs) => dirs.Length > 0;
+		public static bool HasSubdirs(string[] dirs) => dirs.Length > 0;
 
 
 		public static bool LoadserverFolders()
