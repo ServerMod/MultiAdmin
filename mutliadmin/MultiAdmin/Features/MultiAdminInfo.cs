a1a48b1030fc1520cc86313f5252f533e9ea3b71
Dankrushen <henryclose@gmail.com>
2018-05-31 20:30:47 -0400

    Update MultiAdmin info to credit all contributors + remove donation link

--- mutliadmin/MultiAdmin/Features/MultiAdminInfo.cs
+++ mutliadmin/MultiAdmin/Features/MultiAdminInfo.cs
@@ -22,13 +22,11 @@ namespace MultiAdmin.MultiAdmin.Commands
 
 		public void PrintInfo()
 		{
-			Server.Write("MultiAdmin for SCP: Secret Laboratory made by Courtney (Grover_c13), who is not a girl.", ConsoleColor.DarkMagenta);
+			Server.Write("MultiAdmin for SCP: Secret Laboratory made by Courtney (Grover_c13), Dankrushen, PatPeter, and SlenderPlays.", ConsoleColor.DarkMagenta);
 			Server.Write("Heavily modified but based off LocalAdmin by Hubert Moszka.", ConsoleColor.DarkMagenta);
 			Server.Write("You can request LocalAdmin source code at moszka.hubert@gmail.com.", ConsoleColor.DarkMagenta);
 			Server.Write("You can find MultiAdmin source code at https://github.com/Grover-c13/MultiAdmin/.", ConsoleColor.DarkMagenta);
 			Server.Write("Released under CC-BY-SA 4.0", ConsoleColor.DarkMagenta);
-			Server.Write("Do you have too much money? appreciate multiadmin/servermod? consider sending me some!", ConsoleColor.Gray);
-			Server.Write("Paypal: Grover.c13@gmail.com", ConsoleColor.Gray);
 		}
 
 		public override string GetFeatureDescription()
