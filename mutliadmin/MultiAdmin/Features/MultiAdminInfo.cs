50cfd94d7246c6be8d5d9f46af48e5a9f27ff270
Evan <Rnen10@gmail.com>
2018-12-04 08:12:58 +0100

    Honestly feel like this is more accurate.

--- mutliadmin/MultiAdmin/Features/MultiAdminInfo.cs
+++ mutliadmin/MultiAdmin/Features/MultiAdminInfo.cs
@@ -24,8 +24,8 @@ namespace MultiAdmin.MultiAdmin.Commands
 
 		public void PrintInfo()
 		{
-			Server.Write("MultiAdmin for SCP: Secret Laboratory made by Courtney (Grover_c13), Dankrushen, PatPeter, and SlenderPlays.", ConsoleColor.DarkMagenta);
-			Server.Write("Heavily modified but based off LocalAdmin by Hubert Moszka.", ConsoleColor.DarkMagenta);
+			Server.Write("MultiAdmin for SCP: Secret Laboratory made by Courtney (Grover_c13) & Dankrushen.", ConsoleColor.DarkMagenta);
+			Server.Write("Contributors: PatPeter, and SlenderPlays.", ConsoleColor.DarkMagenta);			Server.Write("Heavily modified but based off LocalAdmin by Hubert Moszka.", ConsoleColor.DarkMagenta);
 			Server.Write("You can request LocalAdmin source code at moszka.hubert@gmail.com.", ConsoleColor.DarkMagenta);
 			Server.Write("You can find MultiAdmin source code at https://github.com/Grover-c13/MultiAdmin/.", ConsoleColor.DarkMagenta);
 			Server.Write("Released under CC-BY-SA 4.0", ConsoleColor.DarkMagenta);
