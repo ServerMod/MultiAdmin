a5a6f17be7920a4eeba2b6af9e362b3e778b7e14
Rustam Second_Fry Gubaydullin <secondfry@gmail.com>
2019-01-14 15:09:22 +0300

    Fix input keypresses being cut on linux

--- mutliadmin/MultiAdmin/InputThread.cs
+++ mutliadmin/MultiAdmin/InputThread.cs
@@ -1,45 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace MultiAdmin.MultiAdmin
 {
 	class InputThread
 	{
+		private static readonly char[] separator = { ' ' };
+
 		public static void Write(Server server)
 		{
 			while (!server.IsStopping())
 			{
-				while (!Console.KeyAvailable)
-				{
-					if (server.IsStopping())
-					{
-						return;
-					}
-					Thread.Sleep(server.runOptimized ? 500 : 300);
-				}
 				string message = Console.ReadLine();
-				int cursorTop = Console.CursorTop;
-				Console.SetCursorPosition(0, Console.CursorTop - 1);
-				Console.Write(new string(' ', Console.WindowWidth));
-				server.Write(">>> " + message, ConsoleColor.DarkMagenta, -1);
-				Console.SetCursorPosition(0, cursorTop);
-				string[] messageSplit = message.ToUpper().Split(' ');
-				if (messageSplit.Length > 0)
-				{
-					ICommand command;
-					Boolean callServer = true;
-					server.Commands.TryGetValue(messageSplit[0].ToLower().Trim(), out command);
-					if (command != null)
-					{
-						command.OnCall(messageSplit.Skip(1).Take(messageSplit.Length - 1).ToArray());
-						callServer = command.PassToGame();
-					}
+				server.Write(">>> " + message, ConsoleColor.DarkMagenta);
 
-					if (callServer) server.SendMessage(message);
+				string[] messageSplit = message.ToUpper().Split(InputThread.separator, StringSplitOptions.RemoveEmptyEntries);
+				if (messageSplit.Length == 0)
+				{
+					continue;
+				}
+
+				ICommand command;
+				Boolean callServer = true;
+				server.Commands.TryGetValue(messageSplit[0].ToLower().Trim(), out command);
+				if (command != null)
+				{
+					command.OnCall(messageSplit.Skip(1).Take(messageSplit.Length - 1).ToArray());
+					callServer = command.PassToGame();
+				}
+
+				if (callServer)
+				{
+					server.SendMessage(message);
 				}
 			}
 		}
