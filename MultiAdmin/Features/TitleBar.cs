593c4f312f9957b0626cb6a34b9e2f3485b01cdd
horyu1234 <horyu1234@naver.com>
2019-12-30 14:54:03 +0900

    Change the initial value of the playerCount from -1 to 0

--- MultiAdmin/Features/TitleBar.cs
+++ MultiAdmin/Features/TitleBar.cs
@@ -57,7 +57,7 @@ namespace MultiAdmin.Features
 
 		public override void Init()
 		{
-			playerCount = -1; // -1 for the "server" player, once the server starts this will increase to 0.
+			playerCount = 0;
 			UpdateTitlebar();
 		}
 
