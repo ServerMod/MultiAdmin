b77112082d569520b7c3338a372a5b224e4c9710
Dankrushen <henryclose@gmail.com>
2020-08-31 19:13:16 -0400

    Bump version

--- MultiAdmin/Program.cs
+++ MultiAdmin/Program.cs
@@ -15,7 +15,7 @@ namespace MultiAdmin
 {
 	public static class Program
 	{
-		public const string MaVersion = "3.3.0.4";
+		public const string MaVersion = "3.3.1.0";
 		public const string RecommendedMonoVersion = "5.18";
 
 		private static readonly List<Server> InstantiatedServers = new List<Server>();
