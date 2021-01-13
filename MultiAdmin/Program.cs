3b2963d6fcf9e2fc65c07402bc59062646df7cde
Dankrushen <henryclose@gmail.com>
2021-01-13 15:05:58 -0500

    Bump version & remove RestartNextRound

--- MultiAdmin/Program.cs
+++ MultiAdmin/Program.cs
@@ -15,7 +15,7 @@ namespace MultiAdmin
 {
 	public static class Program
 	{
-		public const string MaVersion = "3.3.1.1";
+		public const string MaVersion = "3.3.1.2";
 		public const string RecommendedMonoVersion = "5.18";
 
 		private static readonly List<Server> InstantiatedServers = new List<Server>();
